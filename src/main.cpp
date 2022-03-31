ee71b7195f762695187bab320a840916e2d0806b
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-03-30 21:20:14 -0300

    fix: solving DV equals zero verification problem

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,18 @@ int main()
 {
   setlocale(LC_ALL, "Portuguese");
 
+  // Declaração de variáveis.
   long long int codigo, somaDosDigitos, soma, D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12;
-  string codigoComoString;
 
   cout << "Informe o código de 13 dígitos: ";
   cin >> codigo;
 
-  codigoComoString = to_string(codigo);
-
-  if (codigoComoString.length() != 13)
-  {
-    cout << "Código possui tamanho inválido!";
-
-    return 0;
-  }
-
+  /*
+    Atribui o valor de um determinado caracter do código
+    para um determinado dígito. Obs.: O D0 corresponde
+    ao 13º dígito do código e o D1 ao 12º dígito, assim
+    por diante.
+  */
   D0 = codigo % 10;
   D1 = codigo / 10 % 10;
   D2 = codigo / 100 % 10;
@@ -36,6 +33,15 @@ int main()
   D11 = codigo / 100000000000 % 10;
   D12 = codigo / 1000000000000 % 10;
 
+  // Verifica o tamanho do código informado.
+  if (D12 == 0 || codigo / 1000000000000 >= 10)
+  {
+    cout << "Código possui tamanho inválido ou se inicia com dígito zero!";
+
+    return 0;
+  }
+
+  // Multiplicação dos dígitos ímpares do código por 3.
   D1 = D1 * 3;
   D3 = D3 * 3;
   D5 = D5 * 3;
@@ -48,7 +54,14 @@ int main()
   soma = ((somaDosDigitos / 10) + 1) * 10;
   soma = soma - somaDosDigitos;
 
-  if (soma == D0 || (soma >= 10 && soma % 10 == D0))
+  // Atribui zero a varíavel soma, caso a mesma seja igual a 10.
+  if (soma == 10)
+  {
+    soma = 0;
+  }
+
+  // Verifica se o valor da soma é igual ao dígito verificador.
+  if (soma == D0)
   {
     cout << "Código confere!";
   }
